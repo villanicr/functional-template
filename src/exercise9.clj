1383f866d46fe6ea849d5dca5a3c865cf4e07f3e
villanicr <villanicr@gmail.com>
2020-12-11 17:37:41 -0300

    Add files via upload

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,11 +1,10 @@
 (ns exercise9)
 
-(defn word-count 
-  "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
-  [x]
-  (throw (Exception. "Not implemented."))
-)
 
-
+(defn word-count [s] 
+  (->> s
+    (clojure.string/lower-case)
+    (re-seq #"\w+")
+    frequencies))
 
 
